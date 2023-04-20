4cd6f9ee18e7c4729c4c66469f4a493d8c1a9445
schse3 <sebastian.schedy@thyssenkrupp-automotive.com>
2023-04-20 16:17:33 +0200

    Using other model, which makes it a bit faster.

--- main2.py
+++ main2.py
@@ -1,18 +1,18 @@
-import json
-
+import copy
 from llama_cpp import Llama
 
 # load the model
 print("Loading model...")
 # llm = Llama("./models/gpt4all_ggml/gpt4all-lora-quantized-ggml.bin")
-llm = Llama("./models/ggml-vicuna-13b-4bit-rev1.bin", n_ctx=4048,
-            last_n_tokens_size=4048)
+llm = Llama("./models/ggml-vicuna-13b-4bit-rev1.bin", n_ctx=1024)
 print("Model loaded.")
-output = llm(
+stream = llm(
     "Question: Tell me about Liechtenstein. Answer:",
     max_tokens=200,
     stop=["\n", "Question:", "Q:"],
-    echo=True
+    stream=True,
 )
 # print the output
-print(json.dumps(output, indent=2))
+for output in stream:
+    completionFragment = copy.deepcopy(output)
+    print(completionFragment["choices"][0]["text"])
